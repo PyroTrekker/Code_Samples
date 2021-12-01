ca6a922a7de3dd192074f123f9a12f7ea4219bbb
PyroTrekker <grahamwenz@gmail.com>
2021-11-30 23:30:12 -0600

    Add Clojue samples

---
+++ Clojure/thread.clj
@@ -0,0 +1,10 @@
+(println "Starting another thread...")
+
+;; Used Thread/sleep to simulate long running process
+(def what-is-the-answer-to-life (future 
+        (Thread/sleep 3000) ;; running for 3 seconds
+        (dotimes [i 10] (println "Thread line " (+ i 1)))))
+(Thread/sleep 10000) ;; Wait 10 seconds for the thread
+(println "Thread execution finished.")
+(println "Goodbye!")
+(shutdown-agents)
